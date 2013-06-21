6b42b6586d6857c7200f3a8008d5d9449ecbc39d
Luka Mirosevic <luka@goonbee.com>
2013-06-21 08:37:48 +0200

    Initial Commit

---
+++ GBLocation/GBLocation.h
@@ -0,0 +1,13 @@
+//
+//  GBLocation.h
+//  GBLocation
+//
+//  Created by Luka Mirosevic on 21/06/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBLocation : NSObject
+
+@end
---
+++ GBLocation/GBLocation.m
@@ -0,0 +1,13 @@
+//
+//  GBLocation.m
+//  GBLocation
+//
+//  Created by Luka Mirosevic on 21/06/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBLocation.h"
+
+@implementation GBLocation
+
+@end
